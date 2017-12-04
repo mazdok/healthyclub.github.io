e6ef320d98fff7756f508216f1dfc0a3aa8dc50e
Paul <grudzhyk777@gmail.com>
2017-12-04 13:42:35 +0200

    add new sections

--- application.js
+++ application.js
@@ -6,4 +6,26 @@ $( document ).ready(function() {
       top = $(id).offset().top;
       $('body, html').animate({scrollTop: top-85}, 1000);
   });
+  // back to top
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 1000, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+  }
 });
